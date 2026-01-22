8f32253d64f797b573356b85497435f2a72613b9
ljankowski <lukasz.jankowski@lppsa.com>
2026-01-22 10:30:35 +0100

    Backend

---
+++ src/main/java/com/silkycoders1/jsystemssilkycodders1/config/AiConfig.java
@@ -0,0 +1,14 @@
+package com.silkycoders1.jsystemssilkycodders1.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.ai.chat.client.ChatClient;
+
+@Configuration
+public class AiConfig {
+
+    @Bean
+    public ChatClient chatClient(ChatClient.Builder builder) {
+        return builder.build();
+    }
+}
